c9e666b90ed448dfc84e231c8eb8a2957967e917
Mahmoud Kamal <144077521+OverStarCode@users.noreply.github.com>
2025-04-24 19:46:10 +0200

    Update db_connect.js

--- backend/config/db_connect.js
+++ backend/config/db_connect.js
@@ -1,24 +1,42 @@
-require('dotenv').config();
+// require('dotenv').config();
+// const mysql = require('mysql');
+
+// const DbConnection = () => {
+//     const connection = mysql.createConnection({
+//         host: process.env.DB_HOSTNAME,
+//         port: process.env.DB_PORT || 3306,
+//         user: process.env.DB_USERNAME,
+//         password: process.env.DB_PASSWORD,
+//         database: process.env.DB_DATABASE
+//     });
+
+//     connection.connect((err) => {
+//         if (err) {
+//             console.error('MySQL connection failed:', err.stack);
+//             return;
+//         }
+//         console.log('Connected to MySQL on Railway!');
+//     });
+
+//     return connection;
+// };
+
+// module.exports = DbConnection;
+
+
 const mysql = require('mysql');
 
-const DbConnection = () => {
-    const connection = mysql.createConnection({
-        host: process.env.DB_HOSTNAME,
-        port: process.env.DB_PORT || 3306,
-        user: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
-    });
+const conn = mysql.createConnection({
+    host: 'nozomi.proxy.rlwy.net',
+    port: 54118,
+    user: 'root',
+    password: 'cSIqoebjuFSzihGnhMZsNRWsSWcoVwOo',
+    database: 'railway'
+});
 
-    connection.connect((err) => {
-        if (err) {
-            console.error('MySQL connection failed:', err.stack);
-            return;
-        }
-        console.log('Connected to MySQL on Railway!');
-    });
-
-    return connection;
-};
-
-module.exports = DbConnection;
+conn.connect((err) => {
+    if (err) {
+        return console.error('Error connecting:', err.message);
+    }
+    console.log('Connection successful!');
+});
