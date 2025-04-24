350831d344bff3a72c7b4e1f11d2ac9c0928bf06
Mahmoud Kamal <144077521+OverStarCode@users.noreply.github.com>
2025-04-24 18:15:32 +0200

    Update firebase.config.js

--- frontend/src/firebase.config.js
+++ frontend/src/firebase.config.js
@@ -1,28 +1,23 @@
 
 
+import { initializeApp ,getApps} from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-// // Initialize Firebase
-// // firebase.js
-// import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+        apiKey: process.env.NEXT_PUBLIC_apiKey,
+        authDomain: process.env.NEXT_PUBLIC_authDomain,
+        projectId: process.env.NEXT_PUBLIC_projectId,
+        storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+        messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+        appId: process.env.NEXT_PUBLIC_appId,
+        measurementId: process.env.NEXT_PUBLIC_measurementId  
+};
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC1PE8lRG3ZFqVH4Q8jnCNVMUSPMdEcBik",
-//   authDomain: "otp-project-4f658.firebaseapp.com",
-//   projectId: "otp-project-4f658",
-//   storageBucket: "otp-project-4f658.firebasestorage.app",
-//   messagingSenderId: "62510676508",
-//   appId: "1:62510676508:web:46d706edcfb460b720bdd9",
-//   measurementId: "G-DBK72QNM4M"
-// };
+// Initialize Firebase app
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-//  const auth = getAuth(app);
-//  export { auth, RecaptchaVerifier, signInWithPhoneNumber };
+// Initialize Firebase Auth
+const auth = getAuth(app);
+
+export { app, auth };
