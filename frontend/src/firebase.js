9dd74f1c2c92f75925db53a3d88f2d9779fbf60a
Mahmoud Kamal <144077521+OverStarCode@users.noreply.github.com>
2025-04-24 18:15:48 +0200

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,23 +1,23 @@
 
 
-// import { initializeApp ,getApps} from "firebase/app";
-// import { getAuth } from "firebase/auth";
+import { initializeApp ,getApps} from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-// // Firebase configuration object (Replace these values with your actual Firebase project values)
-// const firebaseConfig = {
-//     apiKey: "AIzaSyC1PE8lRG3ZFqVH4Q8jnCNVMUSPMdEcBik",
-//     authDomain: "otp-project-4f658.firebaseapp.com",
-//     projectId: "otp-project-4f658",
-//     storageBucket: "otp-project-4f658.firebasestorage.app",
-//     messagingSenderId: "62510676508",
-//     appId: "1:62510676508:web:46d706edcfb460b720bdd9",
-//     measurementId: "G-DBK72QNM4M"
-// };
 
-// // Initialize Firebase app
-// const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const firebaseConfig = {
+        apiKey: process.env.NEXT_PUBLIC_apiKey,
+        authDomain: process.env.NEXT_PUBLIC_authDomain,
+        projectId: process.env.NEXT_PUBLIC_projectId,
+        storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+        messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+        appId: process.env.NEXT_PUBLIC_appId,
+        measurementId: process.env.NEXT_PUBLIC_measurementId  
+};
 
-// // Initialize Firebase Auth
-// const auth = getAuth(app);
+// Initialize Firebase app
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
-// export { app, auth };
+// Initialize Firebase Auth
+const auth = getAuth(app);
+
+export { app, auth };
